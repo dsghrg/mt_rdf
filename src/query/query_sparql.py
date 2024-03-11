4cd7a5c147bf86f1b58fabaf9b9e508d038c887b
dsghrg <gehd@zhaw.ch>
2024-03-11 15:25:35 +0100

    run lsqb

--- src/query/query_sparql.py
+++ src/query/query_sparql.py
@@ -1,16 +1,46 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+import time
 from functools import lru_cache
 
-import time
+from SPARQLWrapper import JSON, SPARQLWrapper
 
+# # @lru_cache(maxsize=1000)
+# def execute_sparql(query, db_id):
+#     """This function executes the sparql query and returns the results"""
+#     sparql = SPARQLWrapper("http://160.85.252.68:7200/repositories/"+db_id.lower())
+#     sparql.setTimeout(60)
+#     sparql.setReturnFormat(JSON)
+#     sparql.setQuery('PREFIX :  <http://unics.cloud/ontology/>\nPREFIX onto: <http://www.ontotext.com/>' + query)
+#     # the previous query as a literal string
+#     try:
+#         start_time = time.time()
+#         results = sparql.query()
+#         execution_time = time.time() - start_time
 
-# @lru_cache(maxsize=1000)
-def execute_sparql(query, db_id):
+#         results = results.convert()
+#         clean_results = []
+#         for row in results['results']['bindings']:
+#             interim_results=[]
+#             for i in range(len(results['head']['vars'])):
+#                 if 'datatype' in row.get(results['head']['vars'][i]).keys():
+#                     value = int(row.get(results['head']['vars'][i])['value'])
+#                     interim_results.append(value)
+#                 else:
+#                     value = row.get(results['head']['vars'][i])['value'] 
+#                     interim_results.append(value)
+#             if len(interim_results) != 0:
+#                 clean_results.append(tuple(interim_results))
+#         return clean_results, execution_time
+#     except Exception as e:
+#         print(e)
+#     return None, 0
+
+def execute_sparql(query):
     """This function executes the sparql query and returns the results"""
-    sparql = SPARQLWrapper("http://160.85.252.68:7200/repositories/"+db_id.lower())
-    sparql.setTimeout(60)
+    sparql = SPARQLWrapper("http://172.17.0.1:9999/sparql")
+    # sparql.setTimeout(300)
     sparql.setReturnFormat(JSON)
-    sparql.setQuery('PREFIX :  <http://unics.cloud/ontology/>\nPREFIX onto: <http://www.ontotext.com/>' + query)
+    # sparql.setQuery('PREFIX :  <http://unics.cloud/ontology/>\nPREFIX onto: <http://www.ontotext.com/>' + query)
+    sparql.setQuery(query)
     # the previous query as a literal string
     try:
         start_time = time.time()
@@ -18,19 +48,20 @@ def execute_sparql(query, db_id):
         execution_time = time.time() - start_time
 
         results = results.convert()
-        clean_results = []
-        for row in results['results']['bindings']:
-            interim_results=[]
-            for i in range(len(results['head']['vars'])):
-                if 'datatype' in row.get(results['head']['vars'][i]).keys():
-                    value = int(row.get(results['head']['vars'][i])['value'])
-                    interim_results.append(value)
-                else:
-                    value = row.get(results['head']['vars'][i])['value'] 
-                    interim_results.append(value)
-            if len(interim_results) != 0:
-                clean_results.append(tuple(interim_results))
-        return clean_results, execution_time
+        return results, execution_time
+    #     clean_results = []
+    #     for row in results['results']['bindings']:
+    #         interim_results=[]
+    #         for i in range(len(results['head']['vars'])):
+    #             if 'datatype' in row.get(results['head']['vars'][i]).keys():
+    #                 value = int(row.get(results['head']['vars'][i])['value'])
+    #                 interim_results.append(value)
+    #             else:
+    #                 value = row.get(results['head']['vars'][i])['value']
+    #                 interim_results.append(value)
+    #         if len(interim_results) != 0:
+    #             clean_results.append(tuple(interim_results))
+    #     return clean_results, execution_time
     except Exception as e:
         print(e)
     return None, 0
---
+++ src/scripts/run_lsqb_queries.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from src.query.query_sparql import execute_sparql
+
+
+def main():
+
+    for filename in os.listdir("data/queries/lsqb/"):
+        with open(f"data/queries/lsqb/{filename}") as f:
+            query = f.read()
+            res, time = execute_sparql(query=query)
+            print(time)
+    # with open("data/queries/lsqb/q1.sparql") as f:
+    #     query = f.read()
+    # get all the slow timeouted queries from old run
+    # df_res = pd.read_csv(res_path + f'results_{qtype.value}.csv')
+    # df_res = df_res[df_res['exec_time'] == 0]
+
+
+    # df = pd.read_csv(f'data/queries/wdbench/{qtype.value}.txt', header=None)
+    # df.rename(columns={0: 'id', 1: 'query_parts'}, inplace=True)
+
+    # df = df[df['id'].isin(df_res['query_id'])]
+    # print(50*'-')
+    # print(qtype.value)
+    # print(50*'-')
+    # run_all_in_df(query_df=df, query_type=qtype.value)
+        
+    #     res, time = execute_sparql(query=query)
+    
+    # print(time)
+
+
+
+if __name__ == "__main__":
+    main()
