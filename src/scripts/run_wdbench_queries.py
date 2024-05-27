0ff27048c4c762529931bfba50b9ac1cd779961e
dsghrg <gehd@zhaw.ch>
2024-05-27 11:01:29 +0200

    minor bug fix

--- src/scripts/run_wdbench_queries.py
+++ src/scripts/run_wdbench_queries.py
@@ -67,7 +67,7 @@ def main(args):
                 res_dict['exec_time'].append(exec_time)
 
             if index % 10 == 0:
-                res_to_logs(result=res_dict)
+                res_to_logs(result=res_dict, forced=args.forced)
 
             index += 1
         res_to_logs(res_dict)
