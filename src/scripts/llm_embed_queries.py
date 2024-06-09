03e05e51177c0a6737e4777cc6700b92a7a07ed8
dsghrg <gehd@zhaw.ch>
2024-06-09 19:02:57 +0200

    llm encoding script

---
+++ src/scripts/llm_embed_queries.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+import torch
+import torch.nn.functional as F
+from dotenv import load_dotenv
+from huggingface_hub import login
+from torch import Tensor
+from tqdm import tqdm
+from transformers import AutoModel, AutoTokenizer
+
+from src.helper.seed_helper import initialize_gpu_seed
+from src.models.config import Config
+
+sys.path.append(os.getcwd())
+import pandas as pd
+
+from src.helper.logging_helper import setup_logging
+
+setup_logging()
+load_dotenv()
+
+
+ACCESS_TOKEN=os.environ.get('HUGGINGFACE_TOKEN')
+
+
+def process_in_batches(model, entries, batch_size, query_prefix, max_length):
+    encoded_queries = []
+    for i in tqdm(range(0, len(entries), batch_size)):
+        batch = entries[i:i + batch_size]
+        encoded_batch = model.encode(batch, instruction=query_prefix, max_length=max_length)
+        encoded_queries.extend(encoded_batch)
+    return encoded_queries
+
+
+def main():
+    device, _ = initialize_gpu_seed(Config.DEFAULT_MODEL_SEED)
+
+    login(token=ACCESS_TOKEN)
+    model = AutoModel.from_pretrained('nvidia/NV-Embed-v1', trust_remote_code=True)
+    model.to(device)
+
+    task_name_to_instruct = {"example": "Given a Natural Language Question based on a SPARQL Query, return '1' if the original SPARQL Query has a faster runtime if you turn off the virtuoso optmizer or '0' otherwise.",}
+    query_prefix = "Instruct: "+task_name_to_instruct["example"]+"\nQuery: "
+    max_length = 300
+    df = pd.read_csv('data/raw/wdbench_nl.csv')
+
+
+    queries = list(df['query'])
+
+    encoded_queries = process_in_batches(model, queries, 15, query_prefix, max_length)
+
+    df['encoding'] = encoded_queries
+    df.to_csv('data/raw/wdbench_nl_encoded.csv', index=False)
+
+    torch.save(encoded_queries, 'data/raw/wdbench_nl_encoding.pt')
+
+
+if __name__ == "__main__":
+    main()
